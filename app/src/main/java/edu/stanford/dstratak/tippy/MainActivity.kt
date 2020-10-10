347548c956c122e0525bedc735444c3fb09b6353
dastratakos <dastratakos@gmail.com>
2020-10-10 01:54:10 -0700

    Add modifications.
    Added a split feature that allows the user to split the total evenly among 1-15 people.
    Added a round up feature that allows the user to round up the total to the nearest
    dollar. This will update the tip and tip % automatically while keeping the base amount.
    When the toggle is switched back to off, the old tip % will be remembered and restored.
    Began working on a currency feature. Currently have a Spinner menu in place that can
    switch between 6 different currencies, but it has no effect on the numbers.

--- app/src/main/java/edu/stanford/dstratak/tippy/MainActivity.kt
+++ app/src/main/java/edu/stanford/dstratak/tippy/MainActivity.kt
@@ -1,17 +1,22 @@
 package edu.stanford.dstratak.tippy
 
+//import android.R
 import android.animation.ArgbEvaluator
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
-import android.widget.SeekBar
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
+import java.text.NumberFormat
+
 
 private const val TAG = "MainActivity"
 private const val INITIAL_TIP_PERCENT = 15
+private const val INITIAL_SPLIT = 1
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,12 +26,33 @@ class MainActivity : AppCompatActivity() {
         seekBarTip.progress = INITIAL_TIP_PERCENT
         tvTipPercent.text = "$INITIAL_TIP_PERCENT%"
         updateTipDescription(INITIAL_TIP_PERCENT)
-        seekBarTip.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
+        tvSplit.text = "$INITIAL_SPLIT"
+        seekBarSplit.progress = INITIAL_SPLIT - 1
+        var dollarString: String = ""
+        var round: Boolean = false
+        var oldTip = INITIAL_TIP_PERCENT
+
+        seekBarTip.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                Log.i(TAG, "onProgressChanged $progress")
+                Log.i(TAG, "onProgressChanged (seekBarTip) $progress")
                 tvTipPercent.text = "$progress%"
                 updateTipDescription(progress)
-                computeTipAndTotal()
+                computeValues(round, seekBarTip.progress)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                sRoundUp.isChecked = false
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+
+        seekBarSplit.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                Log.i(TAG, "onProgressChanged (seekBarSplit) $progress")
+                val adjustedProgress = progress + 1
+                tvSplit.text = "$adjustedProgress"
+                computeValues(round, seekBarTip.progress)
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {}
@@ -34,16 +60,77 @@ class MainActivity : AppCompatActivity() {
             override fun onStopTrackingTouch(seekBar: SeekBar?) {}
         })
 
-        etBase.addTextChangedListener(object: TextWatcher {
+        etBase.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s.toString() != dollarString) {
+                    etBase.removeTextChangedListener(this)
+
+                    val cleanString: String = s?.replace("""[$,.]""".toRegex(), "") ?: ""
+                    val parsed = cleanString.toDouble() / 100
+                    val formatted = getMoneyFromDouble(parsed)
+
+                    dollarString = formatted as String
+                    etBase.setText(formatted)
+                    etBase.setSelection(formatted.length)
+
+                    etBase.addTextChangedListener(this)
+                }
+            }
 
             override fun afterTextChanged(s: Editable?) {
                 Log.i(TAG, "afterTextChanged $s")
-                computeTipAndTotal()
+                sRoundUp.isChecked = false
+                computeValues(round, seekBarTip.progress)
             }
         })
+
+        sRoundUp.setOnCheckedChangeListener { _, isChecked ->
+            round = isChecked
+            if (round) {
+                oldTip = seekBarTip.progress
+                computeValues(round, oldTip)
+            } else {
+                computeValues(round, oldTip)
+            }
+        }
+
+        // Spinner click listener
+        spinner.setOnItemSelectedListener(object: AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                Log.i(TAG, "position: $position, id: $id")
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // TODO
+            }
+
+        })
+
+        // Spinner Drop down elements
+        val currencies: MutableList<String> = ArrayList()
+        currencies.add("USD ($)")
+        currencies.add("EUR (€)")
+        currencies.add("GBP (£)")
+        currencies.add("JPY (¥)")
+        currencies.add("CAD (C$)")
+        currencies.add("CHF (Fr.)")
+
+        // Creating adapter for spinner
+        val dataAdapter = ArrayAdapter(this, R.layout.spinner_item, currencies)
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(R.layout.spinner_dropdown_item)
+
+        // attaching data adapter to spinner
+        spinner.adapter = dataAdapter
+
     }
 
     private fun updateTipDescription(tipPercent: Int) {
@@ -63,18 +150,37 @@ class MainActivity : AppCompatActivity() {
         tvTipDescription.setTextColor(color)
     }
 
-    private fun computeTipAndTotal() {
+    private fun computeValues(round: Boolean, tip: Int) {
         // Get the value of the base and tip percent
-        if (etBase.text.isEmpty()) {
-            tvTipAmount.text = ""
-            tvTotalAmount.text = ""
+        if (etBase.text.toString() == "$0.00") {
+            tvTipAmount.text = "$0.00"
+            tvTotalAmount.text = "$0.00"
+            tvPerPersonAmount.text = "$0.00"
             return
         }
-        val baseAmount = etBase.text.toString().toDouble()
-        val tipPercent = seekBarTip.progress
-        val tipAmount = baseAmount * tipPercent / 100
-        val totalAmount = baseAmount + tipAmount
-        tvTipAmount.text = "%.2f".format(tipAmount)
-        tvTotalAmount.text = "%.2f".format(totalAmount)
+
+        val baseAmount = etBase.text.toString().replace("""[$,]""".toRegex(), "").toDouble()
+        var tipPercent = tip
+        var tipAmount = baseAmount * tipPercent / 100
+        var totalAmount = baseAmount + tipAmount
+
+        if (round) {
+            totalAmount = kotlin.math.ceil(totalAmount)
+            tipAmount = totalAmount - baseAmount
+            tipPercent = (tipAmount / baseAmount * 100).toInt()
+        }
+
+        val split = seekBarSplit.progress + 1
+        val perPersonAmount = totalAmount / split
+
+        seekBarTip.progress = tipPercent
+        tvTipAmount.text = getMoneyFromDouble(tipAmount)
+        tvTotalAmount.text = getMoneyFromDouble(totalAmount)
+        tvPerPersonAmount.text = getMoneyFromDouble(perPersonAmount)
     }
+
+    private fun getMoneyFromDouble(input: Double): CharSequence? {
+        return NumberFormat.getCurrencyInstance().format((input))
+    }
+
 }
